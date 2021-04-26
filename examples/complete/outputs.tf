6f963453034c148375224e4205a7d4475b72902a
Anton Babenko <anton@antonbabenko.com>
2021-04-26 22:24:29 +0200

    feat: Shorten outputs (removing this_)

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,14 +1,14 @@
-output "this_key_pair_key_name" {
+output "key_pair_key_name" {
   description = "The key pair name."
-  value       = module.key_pair.this_key_pair_key_name
+  value       = module.key_pair.key_pair_key_name
 }
 
-output "this_key_pair_key_pair_id" {
+output "key_pair_key_pair_id" {
   description = "The key pair ID."
-  value       = module.key_pair.this_key_pair_key_pair_id
+  value       = module.key_pair.key_pair_key_pair_id
 }
 
-output "this_key_pair_fingerprint" {
+output "key_pair_fingerprint" {
   description = "The MD5 public key fingerprint as specified in section 4 of RFC 4716."
-  value       = module.key_pair.this_key_pair_fingerprint
+  value       = module.key_pair.key_pair_fingerprint
 }
